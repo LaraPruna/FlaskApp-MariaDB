d69a41dc9a2dababbb77183ea5d30095cd34b88a
Lara Pruna <laraprn.12@gmail.com>
2021-11-02 21:35:14 +0100

    He hecho cambios

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, abort, request
+import os, pymysql
 
-@app.route("/",methods=["GET","POST"])
+app = Flask(__name__)
+cursor = None
+
+@app.route('/',methods=["GET"])
 def login():
-	if request.method=="GET":
-		return render_template()
-	user=request.form.get("usuario")
-	pw=request.form.get("contraseña")
-	return
+	return render_template("login.html")
+	
+@app.route('/buscador',methods=["POST"])
+def buscador():
+	conexion = pymysql.connect(
+		host='192.168.1.108',
+		user=request.form.get("usuario"),
+		password=request.form.get("contraseña"),
+		db='viajes')
+	filtro=request.form.get("filtro")
+	with conexion.cursor() as cursor:
+		cursor.execute("SELECT Destino from Viajes")
+		destinos = cursor.fetchall()
+	conexion.close()
+	return render_template("buscador.html",filtro=filtro,destinos=destinos)
+
+port=os.environ["PORT"]
+app.run('0.0.0.0',int(port),debug=True)
